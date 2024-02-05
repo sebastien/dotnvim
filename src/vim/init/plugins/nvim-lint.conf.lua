4e81490ffe5b78c09a1db7be82372b3297529b36
Sébastien Pierre <sebastien.pierre@gmail.com>
2024-02-06 08:30:30 +1300

    [Fix] lint: added more file types

--- src/vim/init/plugins/nvim-lint.conf.lua
+++ src/vim/init/plugins/nvim-lint.conf.lua
@@ -1,7 +1,12 @@
 -- SEE: https://github.com/mfussenegger/nvim-lint
--- require('lint').linters_by_ft = {
---  markdown = {'vale',}
--- }
+require('lint').linters_by_ft = {
+  markdown = {'vale',},
+  python = {'mypy','bandit','codespell',},
+  javascript = {'eslint','deno',},
+  html = {'tidy',},
+  bash = {'shellcheck',},
+  lua = {'luacheck',},
+}
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   callback = function()
     require("lint").try_lint()
