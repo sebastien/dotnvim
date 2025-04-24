4e36a58afa860b7e6c04602fb53b5950568757d1
Sebastien Pierre <sebastien.pierre@gmail.com>
2025-04-24 22:16:14 +1200

    [Fix] deepsource disabled

--- src/vim/init/plugins/nvim-lint.conf.lua
+++ src/vim/init/plugins/nvim-lint.conf.lua
@@ -1,19 +1,21 @@
 -- SEE: https://github.com/mfussenegger/nvim-lint
-local success, err = pcall(function() require("conform") end)
+local success, err = pcall(function()
+	require("conform")
+end)
 if success then
-require('lint').linters_by_ft = {
-  markdown = {'vale',},
-  python = {'mypy','bandit','deepsource'},
-  javascript = {'eslint','deno','deepsource'},
-  sh = {'deepsource'},
-  html = {'tidy',},
-  bash = {'shellcheck',},
-  lua = {'luacheck',},
-}
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    require("lint").try_lint()
-  end,
-})
+	require("lint").linters_by_ft = {
+		markdown = { "vale" },
+		python = { "mypy", "bandit" },
+		javascript = { "eslint", "deno" },
+		sh = { "shellcheck" },
+		html = { "tidy" },
+		bash = { "shellcheck" },
+		lua = { "luacheck" },
+	}
+	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		callback = function()
+			require("lint").try_lint()
+		end,
+	})
 end
 -- EOF
