4b6a0c8a160faf28193467729cd2cacce416cf8b
Sebastien Pierre <sebastien.pierre@gmail.com>
2025-04-24 21:20:00 +1200

    [Fix] blink: Tab

--- src/vim/init/plugins/blink.cmp.conf.lua
+++ src/vim/init/plugins/blink.cmp.conf.lua
@@ -5,12 +5,23 @@
 -- if that doesn't work, the files can be downloaded and installed in
 -- target/release/libblink_cmp_fuzzy.{so,dylib}
 require("blink.cmp").setup({
-	 -- keymap = {
-	 -- 	['<Tab>'] = {'accept'}
-	 -- },
+	-- SEE <https://cmp.saghen.dev/configuration/keymap.html>
+	keymap = {
+		["<Tab>"] = {
+			function(cmp)
+				if cmp.snippet_active() then
+					return cmp.accept()
+				else
+					return cmp.select_and_accept()
+				end
+			end,
+			"snippet_forward",
+			"fallback",
+		},
+	},
 	-- SEE: https://github.com/saghen/blink.cmp/releases/tag/v1.1.1
 	fuzzy = {
-	    implementation = "prefer_rust_with_warning",
-		prebuilt_binaries= { download=true, ignore_version_mismatch=true}
-	}
+		implementation = "prefer_rust_with_warning",
+		prebuilt_binaries = { download = true, ignore_version_mismatch = true },
+	},
 })
