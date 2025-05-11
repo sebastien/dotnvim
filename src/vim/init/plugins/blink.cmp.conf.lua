0037e960f0c2d09587a75a3b29e581f28589359d
Sebastien Pierre <sebastien.pierre@gmail.com>
2025-05-12 09:44:27 +1200

    [Update] plugins: mason 2.0

--- src/vim/init/plugins/blink.cmp.conf.lua
+++ src/vim/init/plugins/blink.cmp.conf.lua
@@ -1,7 +1,7 @@
 -- SEE: https://cmp.saghen.dev/installation.html
 -- NOTES: Need to be compiled by hand,
 -- rustup toolchain install nightly
--- and then env -c ls ~/.config/nvim/pack/minpac/opt/blink.cmp/ cargo build --release
+-- and then env -C ~/.config/nvim/pack/minpac/opt/blink.cmp/ cargo build --release
 -- if that doesn't work, the files can be downloaded and installed in
 -- target/release/libblink_cmp_fuzzy.{so,dylib}
 require("blink.cmp").setup({
